cd13985d4dfe3340026e3f994a05107501d61a44
Ugur Nurcan <33135616+ugurnur@users.noreply.github.com>
2021-08-07 15:33:12 -0400

    integers added for the same lines

--- src/com/mod/MyMethod.java
+++ src/com/mod/MyMethod.java
@@ -5,7 +5,8 @@ public class MyMethod {
     public static void main(String[] args) {
         System.out.println("my main");
         
-        
+        int z = 3;
+        int x = 88;
         
         System.out.println("this is remote repo update");
     }
