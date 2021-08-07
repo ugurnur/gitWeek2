373aad68f81c06f9b647c44e3973070569f78fa4
Ugur <ugur-n@yandex.com>
2021-08-07 14:40:02 -0400

    Second version

--- src/com/mod/Main.java
+++ src/com/mod/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("After initial start, ");
     }
 }
 
-C:\Users\cafedela\IdeaProjects\githubDemo2
---
+++ src/com/mod/MyMethod.java
@@ -0,0 +1,8 @@
+package com.mod;
+
+public class MyMethod {
+
+    public static void main(String[] args) {
+        System.out.println("my main");
+    }
+}
