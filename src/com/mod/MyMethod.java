94d654a36b7fba75463290865e0d06e638d84d6a
Ugur Nurcan <33135616+ugurnur@users.noreply.github.com>
2021-08-07 14:48:02 -0400

    Update MyMethod.java
    remote repo updated on github

--- src/com/mod/MyMethod.java
+++ src/com/mod/MyMethod.java
@@ -4,5 +4,9 @@ public class MyMethod {
 
     public static void main(String[] args) {
         System.out.println("my main");
+        
+        
+        
+        System.out.println("this is remote repo update");
     }
 }
