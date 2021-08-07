192f58c7e4fee6827d7b68d2c554c5b83c4a4286
Ugur <ugur-n@yandex.com>
2021-08-07 15:34:16 -0400

    local update for conflicting lines

--- src/com/mod/MyMethod.java
+++ src/com/mod/MyMethod.java
@@ -5,7 +5,7 @@ public class MyMethod {
     public static void main(String[] args) {
         System.out.println("my main");
         
-        
+        String str = "java";
         
         System.out.println("this is remote repo update");
     }
